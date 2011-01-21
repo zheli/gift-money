cc2588e5c495374203ce91d023da9f7aa5a6fa73
Zhe <linuxcity.jn@gmail.com>
2011-01-21 21:57:51 +0100

    added a new app gift_money

---
+++ web2py/applications/gift_money/models/0.py
@@ -0,0 +1,18 @@
+from gluon.storage import Storage
+settings = Storage()
+
+settings.migrate = True
+settings.title = '\xe5\x8e\x8b\xe5\xb2\x81\xe9\x92\xb1'
+settings.subtitle = 'powered by web2py'
+settings.author = 'Zhe'
+settings.author_email = 'linuxcity.jn@gmail.com'
+settings.keywords = '\xe5\x8e\x8b\xe5\xb2\x81\xe9\x92\xb1'
+settings.description = ''
+settings.layout_theme = 'Default'
+settings.database_uri = 'sqlite://storage.sqlite'
+settings.security_key = '25b9044a-fb75-48a1-8629-61ce7f2b6ceb'
+settings.email_server = 'localhost'
+settings.email_sender = 'you@example.com'
+settings.email_login = ''
+settings.login_method = 'local'
+settings.login_config = ''
