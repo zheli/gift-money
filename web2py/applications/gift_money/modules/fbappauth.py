50fa11e0bc5e621e8582d21a025719fb261feb8c
Zhe <linuxcity.jn@gmail.com>
2011-01-23 00:55:55 +0100

    added some fb sdk stuff

---
+++ web2py/applications/gift_money/modules/fbappauth.py
@@ -0,0 +1,9 @@
+# -*- coding: utf-8 -*-
+FB_AUTH_URL = 'https://graph.facebook.com/oauth/authorize?'
+fb_oae = {
+        'scope' : 'publish_stream',
+        'title' : '壓歲錢',
+        'url'   : 'http://apps.facebook.com/tao_tao',
+        'id'    : '130337413682434',
+        'secret': 'cfe9c6f6d22669f7e91f30c0f60698a5',
+        }
